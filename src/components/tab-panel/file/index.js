d872e0e9f48c0b283be861e42bc88e7644e7ae64
Andrey Filippov <andrey.m.filippov@gmail.com>
2020-04-07 15:20:17 +0800

    refactor: restructure

--- src/components/tab-panel/file/index.js
+++ src/components/tab-panel/file/index.js
@@ -1,11 +1,12 @@
-import Button from '../button'
+import Button from '../../button'
+import style from './style.css'
 
 const PanelFile = ({onClick}) => (
-  <>
+  <div class={style.panel}>
     <h2>drag & drop any files</h2>
     <p>or</p>
     <Button onClick={onClick}>Chose a local file</Button>
-  </>
+  </div>
 )
 
 export default PanelFile
