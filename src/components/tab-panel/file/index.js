19360e9d192e561be1a4aef9735e5b29b18f4f55
Andrey Filippov <andrey.m.filippov@gmail.com>
2020-04-08 09:35:06 +0800

    refactor: minor fix styles

--- src/components/tab-panel/file/index.js
+++ src/components/tab-panel/file/index.js
@@ -3,7 +3,7 @@ import style from './style.css'
 
 const PanelFile = ({onClick}) => (
   <div class={style.panel}>
-    <h2>drag & drop any files</h2>
+    <h2 class={style.title}>drag & drop any files</h2>
     <p>or</p>
     <Button onClick={onClick}>Chose a local file</Button>
   </div>
