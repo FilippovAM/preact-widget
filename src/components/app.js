0e7efeb27321110e364a836af04d8c0e2f9f68ad
Andrey Filippov <andrey.m.filippov@gmail.com>
2020-04-09 15:47:35 +0800

    refactor: move state for widget-tabs to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,34 @@
 import { Component } from 'preact'
 import Button from './button'
 import WidgetModal from './widget-modal'
-import { useContext, useState } from 'preact/compat'
+import { useContext, useEffect, useState } from 'preact/compat'
 import { LocaleStore } from '../store/locale'
 import LanguageSelect from './language-select'
 import WidgetTabs from './widget-tabs'
+import { TabsEnum } from '../constants/tabs'
+
+const DEFAULT_ACTIVE_TAB = TabsEnum.FILE
+const DEFAULT_TABS = Object.values(TabsEnum)
 
 export default class App extends Component {
   render(props) {
     const [isOpen, setIsOpen] = useState()
     const {state: {translate: t}} = useContext(LocaleStore)
+    const [activeTab, setActiveTab] = useState(props.activeTab || DEFAULT_ACTIVE_TAB)
+    const [tabs, setTabs] = useState(props.tabs || DEFAULT_TABS)
+
+    useEffect(() => {
+      props.tabs && setTabs(props.tabs)
+    }, [props.tabs])
+
+    useEffect(() => {
+      props.activeTab && setActiveTab(props.activeTab)
+    }, [props.activeTab])
+
+    const onClickTab = tab => e => {
+      e.preventDefault()
+      setActiveTab(tab)
+    }
 
     return (
       <div id="app">
@@ -18,8 +37,9 @@ export default class App extends Component {
 
         <WidgetModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
           <WidgetTabs
-            activeTab={props.activeTab}
-            tabs={props.tabs}
+            activeTab={activeTab}
+            tabs={tabs}
+            onClick={onClickTab}
           />
         </WidgetModal>
       </div>
